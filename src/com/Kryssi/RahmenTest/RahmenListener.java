39aad1797f5cc564d49f17dbf9871de3fc1231cd
Kryssi79 <KryssiListy@freenet.de>
2013-10-19 21:26:11 +0200

    RahmenListener

--- src/com/Kryssi/RahmenTest/RahmenListener.java
+++ src/com/Kryssi/RahmenTest/RahmenListener.java
@@ -1,8 +1,16 @@
 package com.Kryssi.RahmenTest;
 
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.hanging.HangingPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import com.Kryssi.RahmenTest.main;
 
@@ -17,6 +25,8 @@ public class RahmenListener implements Listener
 	}
 	
 	
+	
+	@EventHandler(priority=EventPriority.NORMAL )
 	public void onBlockRedstone(BlockRedstoneEvent event) 
 	{
 		//wenn: am Block  redstone  gebaut/abgebaut   bzw. Signal 
@@ -24,6 +34,30 @@ public class RahmenListener implements Listener
 		int iBlockID = block.getTypeId();
 		System.out.println(" plugin:  onBlockRedstone()  ");
 	}
+	
+	
+	@EventHandler(ignoreCancelled=true)
+    public void onHangingPlaceEvent(HangingPlaceEvent event) {
+        if (event.getEntity() instanceof ItemFrame) 
+        {
+        	Player player = event.getPlayer();
+        }
+	}
+	
+	
+	@EventHandler(ignoreCancelled=true)
+    public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) 
+	{
+        if (event.getRightClicked() instanceof ItemFrame) 
+        {
+        	//get the block location of the ItemFrame
+            Location itemFrameLocation = event.getRightClicked().getLocation();
+            //cast the right clicked on item into an itemFrame
+            ItemFrame itemFrame = (ItemFrame) event.getRightClicked();
+        }
+	}
+	
+	
 	
 
 }
